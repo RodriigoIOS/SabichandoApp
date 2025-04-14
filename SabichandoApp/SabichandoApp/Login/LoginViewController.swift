674d34f438d68bff7776256627e81e2922eb091c
RodriigoiOS <Rodriigo.dev@outlook.com>
2025-04-14 13:05:43 -0300

    Initial commit, adding ViewCode on this project

---
+++ SabichandoApp/SabichandoApp/Login/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  SabichandoApp
+//
+//  Created by Rodrigo on 14/04/25.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
